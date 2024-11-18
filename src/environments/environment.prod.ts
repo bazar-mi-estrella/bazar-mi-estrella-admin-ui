8a7026c3cfb08dcb20159661fa4a92eab8c424ec
Kevin Perez <kevinperezldev@gmail.com>
2024-11-18 18:14:51 -0500

    format

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
-const HOST="https://cix-tech-mart-api.onrender.com";
+const HOST = "https://cix-tech-mart-api.onrender.com";
 
 export const environment = {
-  API:HOST+"/cix-tech-mart-api/v1",
+  API: HOST + "/cix-tech-mart-api/v1",
   production: true,
   defaultauth: 'fakebackend',
   firebaseConfig: {
